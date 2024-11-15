e0120a9e68cc87bc99a2e7f7872822b30a7ae11f
OrionGift3 <168460082+Shengelaa@users.noreply.github.com>
2024-11-15 19:59:43 +0400

    rr

--- TypeScript.ts
+++ TypeScript.ts
@@ -2,33 +2,51 @@
 
 // მაგალითი 1
 
-let name = "Alice";
+let name: string = "Alice";
 
 // მაგალითი 2
 
-let numbers = [1, 2, 3];
+let numbers: number[] = [1, 2, 3];
 
 // მაგალითი 3
 
-function multiply(a, b) {
+function multiply(a: number, b: number): number {
   return a * b;
 }
 
 // მაგალითი 4
 
-const user = { id: 1, name: "Alice" };
+interface User {
+  id: number;
+  name: string;
+}
+
+const user: User = { id: 1, name: "Alice" };
 
 // მაგალითი 5
 // რისთვის ვიყენებთ არსებული კოდში = Guest-ს :
 
-function greet(name: string = "Guest") {}
+function greet(name: string = "Guest"): void {
+  console.log(`Hello, ${name}!`);
+}
 
 // პასუხი:
 
 // მაგალითი 6
 // დაწერეთ ტიპი რომელიც მოერგება ყველა ქვემოთ ჩამოთვლილ ელემენტს. მაგ: type Config = {
 
-const config1 = {
+interface ConfigOptions {
+  fontSize: number;
+  layout: string | null;
+}
+
+interface Config {
+  theme: string;
+  options?: ConfigOptions;
+  tester?: string;
+}
+
+const config1: Config = {
   theme: "dark",
   options: {
     fontSize: 16,
@@ -36,19 +54,28 @@ const config1 = {
   },
 };
 
-const config2 = {
+const config2: Config = {
   theme: "dark",
 };
 
-const config3 = {
+const config3: Config = {
   tester: "test",
 };
 
 // მაგალითი 7
 // დაწერეთ ტიპი რომელიც მოერგება ყველა ქვემოთ ჩამოთვლილ ელემენტს. მაგ: type MixedArray =
-const array1 = [42, "hello", { name: "Alice" }];
-const array2 = ["apple", true, { isValid: false }];
-const array3 = [];
+
+const array1: (number | string | { name: string })[] = [
+  42,
+  "hello",
+  { name: "Alice" },
+];
+const array2: (string | boolean | { isValid: boolean })[] = [
+  "apple",
+  true,
+  { isValid: false },
+];
+const array3: never[] = [];
 
 // მაგალითი 8
 // აღწერეთ რისი ტიპიზაცია ხდება არსებულ კოდში წერილობით
@@ -58,6 +85,7 @@ type Handler = {
   validate?: (input: string) => boolean;
   log?: () => void;
 };
+
 
 // მაგალითი 9
 
